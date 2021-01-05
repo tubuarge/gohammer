4b54c182d3489f7d26eb4335cd98075c0f4c644e
hemreari <hemreari98@gmail.com>
2021-01-05 15:13:42 +0300

    add store contract and module

--- gohammer/main.go
+++ gohammer/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
+	"crypto/ecdsa"
 	"encoding/json"
 	"fmt"
+	"math/big"
 	"os"
 	"os/signal"
 	"path/filepath"
@@ -10,9 +13,10 @@ import (
 
 	"./config"
 	"./rpc"
-	"./token"
+	"./store"
 
-	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	log "github.com/sirupsen/logrus"
 )
@@ -56,17 +60,44 @@ func main() {
 		log.Fatal(err)
 	}
 
-	newToken, err := token.NewToken(common.HexToAddress("0xed9d02e382b34818e88b88a309c7fe71e65f419d"), conn)
+	privateKey, err := crypto.HexToECDSA("4e77046ba3f699e744acb4a89c36a3ea1158a1bd90a076d36675f4c883864377")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	name, err := newToken.Name(nil)
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("error casting public key to ECDSA")
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+
+	nonce, err := conn.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+	gasPrice, err := conn.SuggestGasPrice(context.Background())
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Token name:", name)
+	auth := bind.NewKeyedTransactor(privateKey)
+	auth.Nonce = big.NewInt(int64(nonce))
+	auth.Value = big.NewInt(0)     // in wei
+	auth.GasLimit = uint64(300000) // in units
+	auth.GasPrice = gasPrice
+
+	input := "1.0"
+	address, tx, instance, err := store.DeployStore(auth, conn, input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(address.Hex())   // 0x147B8eb97fD247D06C4006D269c90C1908Fb5D54
+	fmt.Println(tx.Hash().Hex()) // 0xdae8ba5444eefdc99f4d45cd0c4f24056cba6a02cefbf78066ef9f4188ff7dc0
+
+	_ = instance
 
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
